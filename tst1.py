0f52332b62953e87f0f0a29bf02e1846777d77d7
andre <andrblag@gmail.com>
2025-08-28 19:53:15 +0300

    second commit

--- tst1.py
+++ tst1.py
@@ -5,5 +5,12 @@ def couple(my_list):
             new_list.append(item)
     return new_list
 
+def main():
+    new_list = []
+    for item in range(1, 11):
+        new_list.append(item)
+
+
 list_figures = [2,3,5,6,7,8,9,10]
 print(couple(list_figures))
+print(main())
